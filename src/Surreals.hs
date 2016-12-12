787cc4c5dcf5729413b2df6eb5a5092308961636
Alejandro Alfonso <alejandro@tiendanube.com>
2016-12-12 10:44:50 -0300

    Basic arithmetic

--- src/Surreals.hs
+++ src/Surreals.hs
@@ -15,3 +15,21 @@ instance Eq Surreal where
 
 instance Ord Surreal where
   (<=) = lte
+
+instance Num Surreal where
+  x + y = Surreal left right
+    where
+      left  = map (+y) (l x) ++ map (+x) (l y)
+      right = map (+y) (r x) ++ map (+x) (r y)
+
+  x * y = Surreal (left1++left2) (right1++right2)
+    where
+      left1  = [xl*y + x*yl - xl*yl | xl <- l x, yl <- l y]
+      left2  = [xr*y + x*yr - xr*yr | xr <- r x, yr <- r y]
+      right1  = [xl*y + x*yr - xl*yr | xl <- l x, yr <- r y]
+      right2  = [xr*y + x*yl - xr*yl | xr <- r x, yl <- l y]
+
+  negate (Surreal ls rs) = Surreal (map negate rs) (map negate ls)
+  abs x = error "Not yet implemented"
+  signum x = error "Not yet implemented"
+  fromInteger n = error "Not yet implemented"
--- test/Spec.hs
+++ test/Spec.hs
@@ -9,14 +9,21 @@ _m1 = Surreal [] [_0]
 _m2 = Surreal [] [_m1]
 _m1'2 = Surreal [_m1] [_0]
 
-testsEquality = "Equality" ~: TestList
+testsEquality = "Equality with the same form" ~: TestList
   [    _0 == _0    ~?    "0 = 0"
   ,  _1'2 == _1'2  ~?  "1/2 = 1/2"
   ,    _1 == _1    ~?    "1 = 1"
   ,    _2 == _2    ~?    "2 = 2"
   , _m1'2 == _m1'2 ~? "-1/2 = -1/2"
   ,   _m1 == _m1   ~?   "-1 = -1"
-  ,   _m2 == _m2   ~?   "-2 = -2" ]
+  ,   _m2 == _m2   ~?   "-2 = -2"
+  ]
+
+testsEquality2 = "Equality with different forms" ~: TestList
+  [ Surreal [_m1] [_1] == _0 ~? "{-1|1} = 0"
+  , Surreal [_m2] [_1'2] == _0 ~? "{-2|1/2} = 0"
+  , Surreal [_m2, _m1] [_0, _1, _2] == _m1'2 ~? "{-2,-1|0,1,2} = -1/2"
+  ]
 
 testsOrder = "Order" ~: TestList
   [   _m2 < _m1   ~?   "-2 < -1"
@@ -28,7 +35,33 @@ testsOrder = "Order" ~: TestList
   ,   _m2 < _0    ~?   "-2 < 0"
   ,    _0 < _2    ~?    "0 < 2"
   ,   _m2 < _2    ~?   "-2 < 2"
-  , _m1'2 < _1'2  ~?  "-1/2 < 1/2" ]
+  , _m1'2 < _1'2  ~?  "-1/2 < 1/2"
+  ]
+
+testsAddition = "Addition" ~: TestList
+  [   _0 + _2   == _2        ~?     "0 + 2 = 2"
+  ,   _1 + _m1  == _0        ~?    "1 + -1 = 0"
+  , _1'2 + _1'2 == _1        ~? "1/2 + 1/2 = 1"
+  ,   _1 + _1'2 == _2 - _1'2 ~?   "1 + 1/2 = 2 - 1/2"
+  ]
+
+testsMultiplication = "Multiplication" ~: TestList
+  [    _0 * _2 == _0  ~?   "0 * 2 = 0"
+  ,   _m1 * _2 == _m2 ~?  "-1 * 2 = -2"
+  ,  _1'2 * _2 == _1  ~? "1/2 * 2 = 1"
+  ]
+
+testsArithmetic = "Basic arithmetic" ~: TestList
+  [ _m2 + _1'2 * _2 == _1 - _2 ~?   "-2 + 1/2 * 2 = 1 - 2"
+  , _2 * (_2 + _1) == _2 * _2 + _2 * _1 ~?  "2 * (2+1) = 2*2 + 2*1"
+  ]
 
 main :: IO Counts
-main = runTestTT $ TestList [testsEquality, testsOrder]
+main = runTestTT $ TestList
+  [ testsEquality
+  , testsEquality2
+  , testsOrder
+  , testsAddition
+  , testsMultiplication
+  , testsArithmetic
+  ]
