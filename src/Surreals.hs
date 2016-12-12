f05253500b02ae71503bcec1ffdec210b9e8540c
Alejandro Alfonso <alejandro@tiendanube.com>
2016-12-12 10:52:26 -0300

    Rename leftSet and rightSet for clarity

--- src/Surreals.hs
+++ src/Surreals.hs
@@ -3,11 +3,11 @@ module Surreals
 
 data Surreal = Surreal [Surreal] [Surreal] deriving Show
 
-l (Surreal ls _) = ls
-r (Surreal _ rs) = rs
+leftSet  (Surreal ls _) = ls
+rightSet (Surreal _ rs) = rs
 
 -- x >= y iff no Xᴿ <= y and x <= no yᴸ
-x `gte` y = not $ or $ (map (\xr -> xr `lte` y) (r x)) ++ (map (\yl -> x `lte` yl) (l y))
+x `gte` y = not $ or $ (map (\xr -> xr `lte` y) (rightSet x)) ++ (map (\yl -> x `lte` yl) (leftSet y))
 x `lte` y = y `gte` x
 
 instance Eq Surreal where
@@ -19,15 +19,15 @@ instance Ord Surreal where
 instance Num Surreal where
   x + y = Surreal left right
     where
-      left  = map (+y) (l x) ++ map (+x) (l y)
-      right = map (+y) (r x) ++ map (+x) (r y)
+      left  = map (+y) (leftSet x) ++ map (+x) (leftSet y)
+      right = map (+y) (rightSet x) ++ map (+x) (rightSet y)
 
   x * y = Surreal (left1++left2) (right1++right2)
     where
-      left1  = [xl*y + x*yl - xl*yl | xl <- l x, yl <- l y]
-      left2  = [xr*y + x*yr - xr*yr | xr <- r x, yr <- r y]
-      right1  = [xl*y + x*yr - xl*yr | xl <- l x, yr <- r y]
-      right2  = [xr*y + x*yl - xr*yl | xr <- r x, yl <- l y]
+      left1   = [ xl*y + x*yl - xl*yl | xl <- leftSet x,  yl <- leftSet y  ]
+      left2   = [ xr*y + x*yr - xr*yr | xr <- rightSet x, yr <- rightSet y ]
+      right1  = [ xl*y + x*yr - xl*yr | xl <- leftSet x,  yr <- rightSet y ]
+      right2  = [ xr*y + x*yl - xr*yl | xr <- rightSet x, yl <- leftSet y  ]
 
   negate (Surreal ls rs) = Surreal (map negate rs) (map negate ls)
   abs x = error "Not yet implemented"
